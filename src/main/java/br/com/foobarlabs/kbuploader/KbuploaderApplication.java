a5aabf195d3f888a1cdba9c8f0aabbeb042eaaf3
Roger Vieira <rogerdenis@gmail.com>
2020-09-23 13:43:16 -0300

    Initial structure definition

---
+++ src/main/java/br/com/foobarlabs/kbuploader/KbuploaderApplication.java
@@ -0,0 +1,13 @@
+package br.com.foobarlabs.kbuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbuploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbuploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/foobarlabs/kbuploader/KbuploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foobarlabs.kbuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbuploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
